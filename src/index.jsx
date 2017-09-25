4d3b95460b0eeb456aef57e31f8fd1fe24e376ec
Kamal Sharif <ksharifbd@gmail.com>
2017-09-25 10:08:03 +0600

    fixed eslint identation error

--- src/index.jsx
+++ src/index.jsx
@@ -7,20 +7,20 @@ import '../node_modules/bootstrap/scss/bootstrap';
 const APP = document.getElementById('app');
 
 const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
-    APP
-  );
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,
+        APP
+    );
 };
 
 render();
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./components/..', () => {
-    const NewApp = require('./components/..').default
-    render(NewApp)
-  });
+    module.hot.accept('./components/..', () => {
+        const NewApp = require('./components/..').default
+        render(NewApp)
+    });
 }
