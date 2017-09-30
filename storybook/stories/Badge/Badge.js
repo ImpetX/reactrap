76bbfd7890566b4ce95048b22c63e536cace34e4
Kamal Sharif <ksharifbd@gmail.com>
2017-09-30 06:53:28 +0600

    added storybook info addon

--- storybook/stories/Badge/Badge.js
+++ storybook/stories/Badge/Badge.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
+import {withInfo} from '@storybook/addon-info';
 
 import Badge from '../../../src/components/Badge/Badge.jsx';
 
 storiesOf('Badge', module)
-    .add('default', () => (
-        <Badge
-            label='Default'
-            onClick={action('Badge clicked!')}/>
-    ))
+    .add('default',
+        withInfo('')(() =>
+            <Badge
+                label='Default'
+                onClick={action('Badge clicked!')}/>
+        ))
 
-    .add('with Anchor Tag', () => (
-        <Badge
-            label='Anchor'
-            href='#'
-            target='_blank'/>
-    ))
+    .add('with Anchor Tag',
+        withInfo('')(() =>
+            <Badge
+                label='Anchor'
+                href='#'
+                target='_blank'/>
+        ))
 
-    .add('Rounded', () => (
-        <Badge
-            label='Rounded'
-            pill={true}/>
-    ));
+    .add('Rounded',
+        withInfo('')(() =>
+            <Badge
+                label='Rounded'
+                pill={true}/>
+        ));
